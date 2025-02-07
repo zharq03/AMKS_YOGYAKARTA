a5aa45a46d409685d606c886b7fa05b0b1a64c4a
zharq03 <azhar.rizqullah135@gmail.com>
2025-02-08 00:02:18 +0700

    Update BukuAngkatan.jsx

--- src/pages/BukuAngkatan.jsx
+++ src/pages/BukuAngkatan.jsx
@@ -5,8 +5,8 @@ const BukuAngkatan = () => {
     <>
       <Header />
       <main className="container mx-auto mt-6">
-        <h1 className="text-2xl font-bold">Selamat Datang di Beranda AMKS Yogyakarta</h1>
-        <p className="mt-4 text-gray-600">Konten halaman beranda di sini...</p>
+        <h1 className="text-2xl font-bold">Selamat Datang di Buku Angkatan AMKS Yogyakarta</h1>
+        <h1 className="mt-4 text-gray-100">TOLONG DWI ALDI DI KERJAIN KERJAANNYA</h1>
       </main>
     </>
   );
